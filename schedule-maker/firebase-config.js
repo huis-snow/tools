020dd49093b1cb0689ca09f6dcfdd0caec3c12af
huis-snow <huis.snow@gmail.com>
2026-07-19 13:42:38 +0900

    chore: connect Firebase schedule rooms

--- schedule-maker/firebase-config.js
+++ schedule-maker/firebase-config.js
@@ -4,11 +4,11 @@
   // Firebase Console > 프로젝트 설정 > 내 앱 > 웹 앱의 공개 설정값을 붙여 넣으세요.
   // 이 값들은 프로젝트를 식별하는 공개 웹 설정이며 서비스 계정 키가 아닙니다.
   root.EonjepyoFirebaseConfig = Object.freeze({
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    appId: "",
+    apiKey: "AIzaSyCkCJh5iMqIFULqVMFzbi_MAH-O0XCt-pg",
+    authDomain: "dogoo-a697f.firebaseapp.com",
+    projectId: "dogoo-a697f",
+    appId: "1:9622978431:web:7e0c4a7b21767bedf4244a",
     // App Check를 설정한 뒤 reCAPTCHA Enterprise 사이트 키를 넣습니다. 처음에는 비워도 됩니다.
-    appCheckSiteKey: "",
+    appCheckSiteKey: "6Lcx5VotAAAAAGuZCQIzsK3aCOXZXkVzXYsDvqVw",
   });
 })(typeof globalThis !== "undefined" ? globalThis : this);
